43ae9724225860cc55023cea882c423d6c519b2d
myx99 <26455604@qq.com>
2017-03-07 15:10:18 +0800

    upload change of today

---
+++ Utilities/Query_data_from_MySQL.py
@@ -0,0 +1,26 @@
+import pymysql
+import pandas as pd
+
+def Query_MySQL(fund_id):
+    # init db connection
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='Matao_2012', db='test', use_unicode=True, charset="utf8")
+    cur = conn.cursor()
+
+    # compile sql statement
+    # This is to get the NAV increase rate from db and calculate SHARPE value
+    select_statement = "select Occur_Date as 'Date', Subject_Name as 'NAV_Increase' from Valuation " \
+                       "where Product_ID = '%s' " \
+                       "and Occur_Date > '2017-01-31' and Occur_Date < '2017-03-01' " \
+                       "and Subject_Code = '净值日增长率(%s):' " \
+                       "order by Occur_Date" % (fund_id, '%')
+    # print(select_statement)  # DEBUG
+
+    # use pandas to get the sql result
+    # df = pd.read_sql(sql=select_statement, con=conn, index_col="Date")
+    df = pd.read_sql(sql=select_statement, con=conn)
+    # print(df)  # DEBUG
+    return df
+
+    # close db connection
+    cur.close()
+    conn.close()
